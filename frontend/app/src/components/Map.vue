71ae89fb503992c497c4ef149a8b3d73e1a1b35d
Loris Lukić <loris.lukic13@gmail.com>
2022-09-05 15:29:42 +0200

    Stvaranje mape

--- frontend/app/src/components/Map.vue
+++ frontend/app/src/components/Map.vue
@@ -1,27 +1,33 @@
-<GmapMap
-  :center="{lat:10, lng:10}"
-  :zoom="7"
-  map-type-id="terrain"
-  style="width: 500px; height: 300px"
->
-  <GmapMarker
-    :key="index"
-    v-for="(m, index) in markers"
-    :position="m.position"
-    :clickable="true"
-    :draggable="true"
-    @click="center=m.position"
-  />
-</GmapMap>
+<template>
+  <div style="">
+    <GmapMap
+      :center="{lat: coordinates.lat, lng:coordinates.lng}"
+      :zoom="15"
+      map-type-id="terrain"
+      style="width: 100%; height: 900px; margin: auto"
+    >
+    <GmapMarker
+      :key="index"
+      v-for="(m, index) in markers"
+      :position="m.position"
+      :clickable="true"
+      :draggable="true"
+      @click="center=m.position"
+    />
+    </GmapMap>
+  </div>
+</template>
+
 <script>
-    export default {
-      mounted () {
-        // At this point, the child GmapMap has been mounted, but
-        // its map has not been initialized.
-        // Therefore we need to write mapRef.$mapPromise.then(() => ...)
-     
-        this.$refs.mapRef.$mapPromise.then((map) => {
-          map.panTo({lat: 1.38, lng: 103.80})
-        })
-      }
-    }
+  export default {
+      name: 'Map',
+      data() {
+          return{
+              coordinates: {
+                  lat:0,
+                  lng:0,
+              }
+            }
+          }
+        }
+</script>
--- frontend/app/src/main.js
+++ frontend/app/src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueGeolocation from 'vue-browser-geolocation';
+import * as VueGoogleMaps from 'vue2-google-maps'
+import { BootstrapVue } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+
+Vue.use(VueGeolocation);
+Vue.use(BootstrapVue)
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyCc_CY_DWbBD8xWnZALfvX2Fhi8vTpvZes',
+    libraries: 'places', 
+
+  },
+ 
+})
 
 new Vue({
   router,
