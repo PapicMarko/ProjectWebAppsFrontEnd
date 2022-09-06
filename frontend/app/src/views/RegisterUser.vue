44ffb70bfc4e61999984b4ffc7dbb941c3b75945
Loris Lukić <loris.lukic13@gmail.com>
2022-09-06 19:14:19 +0200

    Finalni objekt, ponovni mali rework i update

---
+++ frontend/app/src/views/RegisterUser.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="register">
+      <RegisterUser />
+    </div>
+  </template>
+  <script>
+  import RegisterUser from "@/components/RegisterUser";
+  export default {
+    components: {
+      RegisterUser
+    }
+  };
+  </script>
